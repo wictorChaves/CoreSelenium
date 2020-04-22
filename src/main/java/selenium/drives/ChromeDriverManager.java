8b4850c76e9cbc3d253416be374a62da62c2f93a
Wictor Chaves <wictor@hotmail.com.br>
2020-04-22 13:41:14 -0300

    Pequenas melhorias e inclusão do driver.

--- src/main/java/selenium/drives/ChromeDriverManager.java
+++ src/main/java/selenium/drives/ChromeDriverManager.java
@@ -1,11 +1,8 @@
 package selenium.drives;
 
 import helper.AppConfigHelper;
-import helper.FileHelper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.io.File;
 
 public class ChromeDriverManager implements DriverManager {
 
@@ -21,24 +18,30 @@ public class ChromeDriverManager implements DriverManager {
     }
 
     private static WebDriver _config(boolean headless) {
+
         System.setProperty("webdriver.chrome.driver", AppConfigHelper.GetProp("drive_executable_path"));
         ChromeOptions options = new ChromeOptions();
 
         if (Boolean.parseBoolean(AppConfigHelper.GetProp("headless")))
             options.addArguments("--headless");
+
         if (Boolean.parseBoolean(AppConfigHelper.GetProp("start_maximized")))
             options.addArguments("--start-maximized");
+
         if (Boolean.parseBoolean(AppConfigHelper.GetProp("auto_open_devtools_for_tabs")))
             options.addArguments("--auto-open-devtools-for-tabs");
 
         String binary = AppConfigHelper.GetProp("binary");
         if (!binary.equals(""))
             options.setBinary(binary);
+
         if (Boolean.parseBoolean(AppConfigHelper.GetProp("disable_extensions")))
             options.addArguments("chrome.switches", "--disable-extensions");
+
         String user_data_dir = AppConfigHelper.GetProp("user_data_dir");
         if (!user_data_dir.equals(""))
             options.setBinary("--user-data-dir=" + user_data_dir);
+
         String profile_directory = AppConfigHelper.GetProp("profile_directory");
         if (!profile_directory.equals(""))
             options.setBinary("--profile-directory=" + profile_directory);
